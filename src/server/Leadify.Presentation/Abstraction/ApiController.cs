edc6b7d0921b5cc6ed2aed7960333283ffbb69f8
bitdevcraft <ryanvincent.capio@outlook.com>
2024-08-21 23:01:01 +0400

    Remove Commented Code

--- src/server/Leadify.Presentation/Abstraction/ApiController.cs
+++ src/server/Leadify.Presentation/Abstraction/ApiController.cs
@@ -2,7 +2,6 @@
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Leadify.Presentation.Abstraction;
 
@@ -13,46 +12,6 @@ public class ApiController : ControllerBase
     internal readonly ISender _sender;
 
     protected ApiController(ISender sender) => _sender = sender;
-
-    /* protected ActionResult Problem(List<Error> errors)
-     {
-         if (errors.Count is 0)
-         {
-             return Problem();
-         }
-
-         if (errors.All(error => error.Type == ErrorType.Validation))
-         {
-             return ValidationProblem(errors);
-         }
-
-         return Problem(errors[0]);
-     }
-
-     private ObjectResult Problem(Error error)
-     {
-         var statusCode = error.Type switch
-         {
-             ErrorType.Conflict => StatusCodes.Status409Conflict,
-             ErrorType.Validation => StatusCodes.Status400BadRequest,
-             ErrorType.NotFound => StatusCodes.Status404NotFound,
-             ErrorType.Unauthorized => StatusCodes.Status403Forbidden,
-             _ => StatusCodes.Status500InternalServerError,
-         };
-
-         return Problem(statusCode: statusCode, title: error.Description);
-     }
-    
-
-    private ActionResult ValidationProblem(List<Error> errors)
-    {
-        var modelStateDictionary = new ModelStateDictionary();
-
-        errors.ForEach(error => modelStateDictionary.AddModelError(error.Code, error.Description));
-
-        return ValidationProblem(modelStateDictionary);
-    }
-     */
 
     protected IActionResult HandleFailure(Result result) =>
         result switch
